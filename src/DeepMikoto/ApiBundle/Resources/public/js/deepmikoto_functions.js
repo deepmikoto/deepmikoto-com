19659f3b9b3f42056d40e7c0e904e4564d657236
alexandru.vasileniuc <deepmikoto@gmail.com>
2015-06-13 12:06:33 +0300

    glgle

--- src/DeepMikoto/ApiBundle/Resources/public/js/deepmikoto_functions.js
+++ src/DeepMikoto/ApiBundle/Resources/public/js/deepmikoto_functions.js
@@ -2,6 +2,14 @@
  * Created by MiKoRiza-OnE on 6/9/2015.
  */
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-56243816-2', 'auto');
+ga('send', 'pageview');
+
 /**
  * here we provide support for symfony app_dev in ajax calls
  */
