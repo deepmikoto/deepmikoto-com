1f84436e5988c8b3ce3c4ea6af342c5930b6fd56
alexandru.vasileniuc <deepmikoto@gmail.com>
2016-11-12 18:56:32 +0200

    sw test

--- src/DeepMikoto/ApiBundle/Resources/public/js/app/_service_worker.js
+++ src/DeepMikoto/ApiBundle/Resources/public/js/app/_service_worker.js
@@ -4,25 +4,15 @@
 'use strict';
 self.addEventListener('push', function(event)
 {
-    console.log('[Service Worker] Push Received.');
-    console.log('[Service Worker] Push had this data: "${event.data.text()}"');
-
-    console.log( event.data );
-    console.log( event.data.text() );
-    const title = 'Push Codelab';
-    const options = {
-        body: 'Yay it works.',
-        icon: 'images/icon.png',
-        badge: 'images/badge.png'
-    };
-
-    event.waitUntil(self.registration.showNotification(title, options));
+    var data = JSON.parse( event.data.text() );
+    event.waitUntil(
+        self.registration.showNotification(data.title, data)
+    );
 });
 
 self.addEventListener('notificationclick', function(event)
 {
     event.notification.close();
-
     event.waitUntil(
         clients.openWindow('https://deepmikoto.com')
     );
